05cd1ddfe49b32ee4b98058218fb172d31cb4f21
Megalitum <fatvlad.95@gmail.com>
2016-03-28 01:58:11 +0300

    Updated MainWindow class.

--- gui/main_form.py
+++ gui/main_form.py
@@ -1,17 +1,41 @@
 # coding: utf8
 
-from PyQt5.QtGui import QPixmap
+
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QDialog
 from PyQt5.uic import loadUiType
+from PyQt5.QtGui import QPixmap, QImage
+
+from graphviz import Digraph
+
 import os.path
 
 form_class, base_class = loadUiType(os.path.join(os.path.dirname(__file__), 'main_form.ui'))
 
 
-class MainWindow(QDialog, form_class):
+class MainWindow(QDialog):
     def __init__(self, *args):
         super(MainWindow, self).__init__(*args)
+        self.ui = form_class()
+        self.ui.setupUi(self)
+        self.graph = Digraph(comment='Когнитивная карта', name='Cognitive map', format='png')
+        self.labels = None
+        self.matrix = None
 
-        self.setupUi(self)
-        import Graphviz
-        self.graphLabel.setPixmap(QPixmap.fromImage(Graphviz.img))
+    def load_labels(self, path):
+        pass
+
+    def load_matrix(self, path):
+        pass
+
+    @pyqtSlot(int)
+    def pageChanged(self, page):
+        if page == 1:
+            self.render_graph()
+
+    def render_graph(self):
+        if not self.matrix:
+            return
+        self.graph.node('A', 'King Arthur', color='blue')
+        img = QImage.fromData(self.graph.pipe(), "png")
+        self.ui.graphLabel.setPixmap(QPixmap.fromImage(img))
