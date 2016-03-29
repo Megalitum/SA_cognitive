a7c7b1090bdae296791c6abec2e20d36fde3414c
Megalitum <fatvlad.95@gmail.com>
2016-03-29 12:20:45 +0300

    Modified graph interface.

--- gui/main_form.py
+++ gui/main_form.py
@@ -25,12 +25,16 @@ class MainWindow(QDialog):
         self.matrix = None
 
     def load_labels(self, path):
+        self.labels = list()
         with open(path, 'r') as label_file:
             csv_rdr = reader(label_file)
-            self.labels = list()
             header = next(csv_rdr)
             for row in csv_rdr:
                 self.labels.append(row[0])
+
+    def __fill_labels(self):
+        if self.labels is not None and self.matrix is not None:
+            self.labels.extend('add' + str(i) for i in range(0, max(self.matrix.shape[0] - len(self.labels), 0)))
 
     def load_matrix(self, path):
         self.matrix = np.load(path)
@@ -40,11 +44,23 @@ class MainWindow(QDialog):
         if page == 1:
             self.render_graph()
 
+    def render_table(self):
+        if self.matrix is None or self.matrix.shape[0] != self.matrix.shape[1]:
+            return
+        if self.labels is None or len(self.labels) != self.matrix.shape[0]:
+            return
+        self.__fill_labels()
+        self.ui.tableWidget.setRowCount(self.matrix.shape[0])
+        self.ui.tableWidget.setColumnCount(self.matrix.shape[1])
+        self.ui.tableWidget.setHorizontalHeaderLabels(self.labels)
+        self.ui.tableWidget.setVerticalHeaderLabels(self.labels)
+
     def render_graph(self):
         if self.matrix is None or self.matrix.shape[0] != self.matrix.shape[1]:
             return
         if self.labels is None or len(self.labels) != self.matrix.shape[0]:
             return
+        self.__fill_labels()
         self.graph = Digraph(comment='Когнитивная карта', name='Cognitive map', format='png')
         for i, label in enumerate(self.labels):
             self.graph.node(str(i), label, color='blue')
